87fbf142b8352c47c0a864b22b953b393a927367
Ken Takahashi <takahashi.ken.716@gmail.com>
2020-04-11 13:13:20 +0700

    Prepare for deployment

--- client/src/actions/postActionCreator.js
+++ client/src/actions/postActionCreator.js
@@ -2,10 +2,13 @@ import axios from "axios";
 import { setAlert } from "./alertActionCreator";
 import {
   GET_POSTS,
+  GET_POST,
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
-  ADD_POST
+  ADD_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from "./types.js";
 
 // Get Posts
@@ -70,7 +73,7 @@ export const deletePost = id => async dispatch => {
       type: DELETE_POST,
       payload: id
     });
-    dispatch(setAlert("Post Removed", "Sucess"));
+    dispatch(setAlert("Post Removed", "Success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
@@ -96,7 +99,73 @@ export const addPost = formData => async dispatch => {
       type: ADD_POST,
       payload: res.data
     });
-    dispatch(setAlert("Post Created", "Sucess"));
+    dispatch(setAlert("Post Created", "Success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status
+      }
+    });
+  }
+};
+
+// Get Post
+export const getPost = id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+    dispatch({
+      type: GET_POST,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Add comment
+export const addComment = (postId, formData) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    const res = await axios.post(
+      `/api/posts/comment/${postId}`,
+      formData,
+      config
+    );
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data
+    });
+    dispatch(setAlert("Comment Added", "Success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status
+      }
+    });
+  }
+};
+
+// Delete comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId
+    });
+    dispatch(setAlert("Comment Added", "Success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
